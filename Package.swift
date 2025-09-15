ee90016ff0a9bc8098010f57095ce82e479b07e7
gzerad <dfedoseyev@gmail.com>
2025-09-15 14:34:12 +0300

    1.0.1 Release

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HMSBroadcastExtensionSDK",
-            url: "https://github.com/100mslive/100ms-ios-broadcast-sdk/releases/download/1.0.0/HMSBroadcastExtensionSDK.xcframework.zip",
-            checksum: "589a000dfdc948f938482d8affb333644ccc74300e5c7df2ea1aa887a94ae0b9"
+            url: "https://github.com/100mslive/100ms-ios-broadcast-sdk/releases/download/1.0.1/HMSBroadcastExtensionSDK.xcframework.zip",
+            checksum: "0f50f8e8b3ba24ea7a7271480e04b653c57970c7126d9704f646180f5856d937"
         )
     ]
 )
